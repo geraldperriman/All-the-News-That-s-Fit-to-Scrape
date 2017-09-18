7296d4219eb24f92a7fa3e744c3350888f0df223
Gerald Perriman <geraldperriman@gmail.com>
2017-09-17 20:11:58 -0400

    m

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var PORT = process.env.PORT || 3000;
 
 // Use body parser with our app
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 // override with POST having ?_method=PUT
@@ -39,11 +39,11 @@ app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
 
-var databaseUri = "mongodb://localhost/nhlscrape";
+var databaseUri = "mongodb://localhost/nflscrape";
 if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect(databaseUri);
+    mongoose.connect(databaseUri);
 }
 var db = mongoose.connection;
 
@@ -52,126 +52,121 @@ var db = mongoose.connection;*/
 
 // Show any mongoose errors
 db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+    console.log("Mongoose Error: ", error);
 });
 
 // Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
-  console.log("Mongoose connection successful.");
+    console.log("Mongoose connection successful.");
 });
 
 //=========Routes==========//
 
-app.get("/", function (req, res) {
-  Article.find({})
-    .exec(function (error, data) {
-      if (error) {
-        res.send(error);
-      }
-      else {
-        var newsObj = {
-          Article: data
-        };
-        res.render("index", newsObj);
-      }
-    });
+app.get("/", function(req, res) {
+    Article.find({})
+        .exec(function(error, data) {
+            if (error) {
+                res.send(error);
+            } else {
+                var newsObj = {
+                    Article: data
+                };
+                res.render("index", newsObj);
+            }
+        });
 });
 
 // A GET request to scrape the nhl/oilers website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
-  request("https://www.nhl.com/oilers/", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h4.headline-link").each(function(i, element) {
+    // First, we grab the body of the html with request
+    request("http://bleacherreport.com/atlanta-falcons", function(error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("h4.headline-link").each(function(i, element) {
 
-      // Save an empty result object
-      var result = {};
+            // Save an empty result object
+            var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).text();
-      result.link = $(this).parent("a").attr("href");
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).text();
+            result.link = $(this).parent("a").attr("href");
 
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
+            // Using our Article model, create a new entry
+            // This effectively passes the result object to the entry (and the title and link)
+            var entry = new Article(result);
 
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
+            // Now, save that entry to the db
+            entry.save(function(err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
 
-    });
-    res.redirect("/");
-    console.log("Successfully Scraped");
-  });
-});
-
-app.post("/notes/:id", function (req, res) {
-  var newNote = new Note(req.body);
-  newNote.save(function (error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      console.log("this is the DOC " + doc);
-      Article.findOneAndUpdate({
-        "_id": req.params.id
-      },
-        { $push: { "note": doc._id } }, {new: true},  function (err, doc) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("note saved: " + doc);
-            res.redirect("/notes/" + req.params.id);
-          }
         });
-    }
-  });
-});
-
-app.get("/notes/:id", function (req, res) {
-  console.log("This is the req.params: " + req.params.id);
-  Article.find({
-    "_id": req.params.id
-  }).populate("note")
-    .exec(function (error, doc) {
-      if (error) {
-        console.log(error);
-      }
-      else {
-        var notesObj = {
-          Article: doc
-        };
-        console.log(notesObj);
-        res.render("notes", notesObj);
-      }
+        res.redirect("/");
+        console.log("Successfully Scraped");
     });
 });
 
-app.get("/delete/:id", function (req, res) {
-  Note.remove({
-    "_id":req.params.id
-  }).exec(function (error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      console.log("note deleted");
-      res.redirect("/" );
-    }
-  });
+app.post("/notes/:id", function(req, res) {
+    var newNote = new Note(req.body);
+    newNote.save(function(error, doc) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("this is the DOC " + doc);
+            Article.findOneAndUpdate({
+                "_id": req.params.id
+            }, { $push: { "note": doc._id } }, { new: true }, function(err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("note saved: " + doc);
+                    res.redirect("/notes/" + req.params.id);
+                }
+            });
+        }
+    });
+});
+
+app.get("/notes/:id", function(req, res) {
+    console.log("This is the req.params: " + req.params.id);
+    Article.find({
+            "_id": req.params.id
+        }).populate("note")
+        .exec(function(error, doc) {
+            if (error) {
+                console.log(error);
+            } else {
+                var notesObj = {
+                    Article: doc
+                };
+                console.log(notesObj);
+                res.render("notes", notesObj);
+            }
+        });
+});
+
+app.get("/delete/:id", function(req, res) {
+    Note.remove({
+        "_id": req.params.id
+    }).exec(function(error, doc) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("note deleted");
+            res.redirect("/");
+        }
+    });
 });
 
 // Listen on port 3000
 app.listen(PORT, function() {
-  console.log("App running on PORT" + PORT + "!");
+    console.log("App running on PORT" + PORT + "!");
 });
